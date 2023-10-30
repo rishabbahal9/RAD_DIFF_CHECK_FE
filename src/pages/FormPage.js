a0b15269b61b04579fb71d59154473a760160886
Rishab Bahal <rishabbahal@gmail.com>
2023-10-30 14:21:42 -0400

    report service made with temp data, data flow created between form and service

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,15 +1,44 @@
 import React from "react";
 import { Container, TextField, Button } from "@mui/material";
+import { useForm } from "react-hook-form";
+
+import * as reportService from "./../services/report";
+
+import * as logger from "./../utils/logger";
 
 const FormPage = (props) => {
+  //Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const handleSubmitForm = async (formData) => {
+    logger.log("HandleSubmitMethod");
+    logger.log(formData);
+    const templateReport = await reportService.getTemplateFromId(
+      formData.templateId
+    );
+    logger.log(templateReport);
+  };
+
   return (
     <>
-      <Container style={{marginTop: "20px"}}>
+      <Container
+        style={{ marginTop: "20px" }}
+        component="form"
+        onSubmit={handleSubmit((data) => handleSubmitForm(data))}
+      >
         <TextField
           id="outlined-basic"
           label="Template Id"
           variant="outlined"
           fullWidth
+          {...register("templateId", { required: true })}
+          error={Boolean(errors.templateId)}
+          helperText={errors.templateId ? "Error: TemplateId required" : ""}
         />
         <TextField
           id="outlined-multiline-flexible"
@@ -17,9 +46,31 @@ const FormPage = (props) => {
           multiline
           rows={20}
           fullWidth
-          style={{marginTop: "20px"}}
+          style={{ marginTop: "20px" }}
+          {...register("userReportContent", { required: true })}
+          error={Boolean(errors.userReportContent)}
+          helperText={
+            errors.userReportContent ? "Error: Report content required" : ""
+          }
         />
-        <Button style={{marginTop: "20px", textAlign: "left"}} variant="contained">Submit</Button>
+
+        <Button
+          style={{ marginTop: "20px", textAlign: "left" }}
+          variant="contained"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          style={{ margin: "auto", marginTop: "20px", textAlign: "left" }}
+          variant="outlined"
+          onClick={() => {
+            reset();
+          }}
+        >
+          Reset
+        </Button>
       </Container>
     </>
   );
