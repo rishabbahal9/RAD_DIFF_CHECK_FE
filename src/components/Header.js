18e2675615aa1fe0dfd6ebb04610ad2b5cc0e219
Rishab Bahal <rishabbahal@gmail.com>
2023-10-31 13:30:37 -0400

    Dockerized react app

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,25 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 
 const Header = (props) => (
   <AppBar position="static">
-    <Toolbar variant="dense" >
+    <Toolbar variant="dense">
       <Typography variant="h6" color="inherit" component="div">
         Radpair Assessment
       </Typography>
+      <div
+        style={{
+          display: "block",
+          padding: "5px",
+          backgroundColor: "green",
+          opacity: "0.3",
+          color: "white",
+          marginLeft: "15px",
+          textAlign: "right",
+          borderRadius: "5px",
+          fontSize: "12px",
+        }}
+      >
+        {process.env.REACT_APP_ENV}
+      </div>
     </Toolbar>
   </AppBar>
 );
